9c9ab7a499f7ab1d4d7616b60502942331b93b3c
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-06-23 16:10:58 -0700

    debugging error where db object not recognized in db session for seed_db.

--- services/web/manage.py
+++ services/web/manage.py
@@ -12,6 +12,39 @@ def create_db():
     db.create_all()
     db.session.commit()
 
+# To use this command:
+# docker-compose exec web python manage.py create_db
+
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(User(email="user@example.com"))
+    db.session.commit()
+
+# To use this command:
+# docker-compose exec web python manage.py seed_db
+
+
+# This did not reveal any method in session which could list or show all objects. I am debugging based on User not
+# seeming to be recognized as a 'name' in the session, like as if the class/table was never defined.
+@cli.command("db_debug")
+def db_session_debug():
+    print()
+    print()
+    print(help(db.session))
+    print()
+    print()
+    print()
+    print()
+    dir(db.session)
+    print()
+    print()
+    print()
+    print()
+    print(db.session)
+    print()
+    print()
+
 
 if __name__ == "__main__":
     cli()
