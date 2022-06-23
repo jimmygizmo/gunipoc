5853372f96dfd05a3f75f932e4df7d1e3d3294ea
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-06-23 14:48:39 -0700

    new web entrypoint script works. waits for db to be up. new env vars for python db code. db detexts persisted db on startup and only recreates if missing. all good so far.

---
+++ services/web/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This script requires nc (netcat) to be installed on the host (container)
+# In this case it is a lightweight Python container.
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Web entrypoint script: Waiting for PostgreSQL port to go to LISTEN state ..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "Web entrypoint script: PostgreSQL started"
+fi
+
+python manage.py create_db
+
+exec "$@"
+
