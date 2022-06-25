15d4ce983970508a072a5979adde8078fbd4dcb1
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-06-25 15:14:38 -0700

    fixed config issue in manage.py and added a lot of related comments.

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,11 +1,39 @@
 from flask.cli import FlaskGroup
-from project import app, db, User, config
+# from project import app, db, User, config  # Importing config here was experimental.
+from project import app, db, User  # We can get config the Flask way through app.
+
+# If we wanted shorter config naming:
+# from project import config.Config as cfg  # This does not work. Bad syntax.
+# Cannot say config.Config here. ANYHOW, Flask has it's own config method.
+# Let's try what the app is already doing, although not in this file yet.
+# (We remove the import of 'config' that did work for this :
+# config.Config.SQLALCHEMY_DATABASE_URI but is not necessarily the way we want.)
+# Flask way. Will try:
+# app.config["SQLALCHEMY_DATABASE_URI"]
+
 
 #### DEBUG 2
 # Adding the following to try to inspect the DB/tables from here
 from sqlalchemy import create_engine
 #engine = create_engine("postgresql://hello_flask:hello_flask@db:5432/hello_flask_prod")
-engine = create_engine(config.Config.SQLALCHEMY_DATABASE_URI)
+
+#engine = create_engine(config.Config.SQLALCHEMY_DATABASE_URI)
+# Shorter variant using variant import above:
+#engine = create_engine(cfg.SQLALCHEMY_DATABASE_URI)  # Did not work out at import.
+
+# This works. TODO: Research pro/cons of doing config the Flask way like this.
+# I have my own clean/lightweight strategy which may or may not be a good way as I
+# am still assessing and refining it, but it involves using just the namespace and
+# not even creating a class, but rather just a dictionary called settings in a
+# global config namespace. There are many ways to do it. I will give the full Flask
+# methodology a study and a fair go before choosing a method for the Python app code
+# of Nucleus. If you App code would benefit from a non-Flask configuration strategy
+# then don't be afraid to do it. Have good reasons though. But you are not required
+# to use any feature of a library, but you are required to understand why or why not,
+# at least if you want to be a good developer you need good justifications based
+# on research, testing, experience and standards appreciation.
+engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])  # The Flask config way.
+
 
 from sqlalchemy import inspect
 inspector = inspect(engine)
--- services/web/project/config.py
+++ services/web/project/config.py
@@ -9,3 +9,14 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
 
+
+# Sidenote: We'll store an application secret in ENV (.env files at least and
+# probably also in a cloud service for secrets like GCP ConfigMap/Secrets.)
+# This can be used with others for signing/securing anything in the app. It must
+# never go into a repo and must never be revealed anywhere.
+# One way to generate a random secret:
+# python -c 'import secrets; print(secrets.token_hex())'
+#
+# Idea: We probably want a distinct .env file just for secrets and will want one
+# for different environments etc.
+#
