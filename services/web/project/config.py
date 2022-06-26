655cd9af6b41c7e9dbdf4ace9cf59a67714fede0
jimmygizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-06-26 01:24:07 -0700

    nginx now serves static. flask serves app. dev vs prod stacks different and both work great.

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -10,13 +10,21 @@ class Config(object):
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
 
 
-# Sidenote: We'll store an application secret in ENV (.env files at least and
+# Side-note: We'll store an application secret in ENV (.env files at least and
 # probably also in a cloud service for secrets like GCP ConfigMap/Secrets.)
 # This can be used with others for signing/securing anything in the app. It must
-# never go into a repo and must never be revealed anywhere.
-# One way to generate a random secret:
+# never go into a repo and must never be revealed anywhere unintentionally.
+# One way to generate a random secret we could use:
 # python -c 'import secrets; print(secrets.token_hex())'
 #
 # Idea: We probably want a distinct .env file just for secrets and will want one
 # for different environments etc.
 #
+
+# This is a good section to jump to about Flask config. RE the prefix feature
+# and including valid json. I think we will use the ENV and not and python/cfg files.
+# https://flask.palletsprojects.com/en/2.1.x/config/#configuring-from-environment-variables
+
+# More good tips further down that page:
+# https://flask.palletsprojects.com/en/2.1.x/config/#development-production
+
